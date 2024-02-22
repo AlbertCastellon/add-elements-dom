c751544d9e57fd87e2ed0779e140f5041e114b50
ACastellon <albertkstyon@gmail.com>
2024-02-22 17:09:04 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
  // Aquí tu código
+const agregar = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+agregar.addEventListener('click', function() {
+    const elemento = prompt('introduce el producto');
+    const objeto = document.createElement('li');
+    objeto.textContent = elemento;
+    lista.appendChild(objeto);
+})
